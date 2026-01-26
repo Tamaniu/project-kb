d54074d3331c1c06c18e285d02af70f8b7c9970c
Tamaniu <54681265+Tamaniu@users.noreply.github.com>
2026-01-26 21:06:27 +0000

    Add WSAPI 2023.3 new features

---
+++ mediacentral/mediacentral/newsroom-management/wsapi/2023.3/new-features.m
@@ -0,0 +1,15 @@
+# New Features in WSAPI 2023.3
+
+## Web Server API
+
+### More secure platform
+
+This version of WSAPI is qualified to be used with **Tomcat 10**, which deploys:
+
+- Servlet 5.0  
+- JSP 3.0  
+- EL 4.0  
+- JWS 2.0  
+- JSAPIC 2.0  
+
+This provides a newer, more secure platform for the Web Services API.
